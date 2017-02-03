38a585fdf10017835751bbe498e703d0e541b91a
Luyu Sun <lysun@login-2.deepthought2.umd.edu>
2017-02-03 16:11:57 -0500

    update obsop

--- obsop_drifters.f90
+++ obsop_drifters.f90
@@ -1,17 +1,18 @@
 PROGRAM obsop_drifters
   
   USE common ! use r_size 
-  USE common_obs_mom4 ! use get_nobs   
-  USE common_mom4
+  USE common_obs_oceanmodel ! use get_nobs   
+  USE common_oceanmodel
   USE letkf_drifters_local, ONLY : obs_local  !(DRIFTERS) NOTE: this is a slightly modified version of letkf_local.f90
   USE letkf_drifters_tools
   USE params_letkf
   USE params_model
   USE params_obs
+  USE vars_model
 
   IMPLICIT NONE
   CHARACTER(slen) :: obsinfile = 'obsin_drifters.dat'	!IN (default) observation data of drifters
-  CHARACTER(slen) :: guesfile = 'gues.drifters_inp.txt'	!IN data from model space in folder "DRIFTERS"
+  CHARACTER(slen) :: guesfile = 'gues'	!IN data from model space in folder "DRIFTERS"
   CHARACTER(slen) :: obsoutfile = 'obsout_drifters.dat'	!OUT (default) datafile to be passed to letkf
   
   REAL(r_size), ALLOCATABLE :: elem(:)	!elem(:)
@@ -31,13 +32,14 @@ PROGRAM obsop_drifters
   !-----------------------------------------------------------------------------
   ! Initialize the common_mom4 module, and process command line options
   !-----------------------------------------------------------------------------
-  CALL set_common_drifters 
+   CALL set_common_oceanmodel
+  !CALL set_common_drifters 
   !CALL process_command_line ! ??, what does this for should I write the subroutine
 
   !-----------------------------------------------------------------------------
   ! Read observations from file
   !-----------------------------------------------------------------------------
-  CALL get_nobs(obsinfile,6,nnobs) !Later, need to add time.
+  CALL get_nobs(obsinfile,7,nnobs) !Later, need to add time.
   
   ALLOCATE(  elem(nnobs)  )
   ALLOCATE(  rlon(nnobs)  )
@@ -48,15 +50,17 @@ PROGRAM obsop_drifters
   ALLOCATE(   ohx(nnobs)  ) ! Model forecast transformed to observation space: H(xb)
   ALLOCATE(   oqc(nnobs)  )  
   ALLOCATE(  otime(nnobs) ) ! otime
-  CALL read_obs2_drifters(trim(obsinfile),nnobs,elem,rlon,rlat,rlev,obid,oerr,otime)
-
+  print *, 'Finish get_nobs and start read_obs2_drifters.'
+  CALL read_obs2_drifters(obsinfile,nnobs,elem,rlon,rlat,rlev,obid,oerr,otime)
+  print *, 'Start read_drifters'
   !-----------------------------------------------------------------------------
   ! Read model forecast for this member
   !-----------------------------------------------------------------------------
-  ALLOCATE( v4d_all(num_drifters,num_times,nv4d)  )
-  CALL read_drifters(trim(guesfile),v4d_all) ! read_drifters is in letkf_drifters.f90, read data from model space.
+  CALL read_dimension(trim(guesfile),num_drifters,num_times)
+  ALLOCATE(v4d_all(num_drifters,num_times,nv4d))
+  CALL read_drifters(trim(guesfile),v4d_all) ! read_drifters is in letkf_drifters_tools.f90, read data from model space.
   ! From this step, we obtain from models, num_drifters, num_times, drifter_ids, drifter_times. 
-
+  print *, 'Finish read_drifters'
   !-----------------------------------------------------------------------------
   ! Cycle through all observations
   !-----------------------------------------------------------------------------
@@ -65,17 +69,20 @@ PROGRAM obsop_drifters
     !---------------------------------------------------------------------------
     ! Convert the physical coordinate to model grid coordinate (note: real, not integer)
     !---------------------------------------------------------------------------
+    !print *, elem(n),obid(n),otime(n),rlon(n),rlat(n),rlev(n)
+    !print *, 'Start drift2ijk'
+    !print *, 'otime(n)=', otime(n)
     CALL drift2ijk(elem(n),obid(n),otime(n),rlon(n),rlat(n),rlev(n),ri,rj,rk) 
+    !print *, 'Finish drift2ijk'
+    print *, 'rj = ', rj
+    print *, obid(n), '=', ri
+    print *, 'rk = ', rk
     ! ri: drifter id index
     ! rj: time idex for interpolation
     ! rk: var (x,y,x) index if elem == iv4d_x,y,z, then rk = 1,2,3 so on
-
+    !print *, elem(n),obid(n),otime(n),rlon(n),rlat(n),rlev(n),ri,rj,rk
     !---------------------------------------------------------------------------
     ! Filter in the tripolar region until localization is examined in the arctic !(ISSUE)
-    !---------------------------------------------------------------------------
-
-    !---------------------------------------------------------------------------
-    ! Filter out observations that are out of range for the grid
     !---------------------------------------------------------------------------
 
     !---------------------------------------------------------------------------
@@ -85,8 +92,28 @@ PROGRAM obsop_drifters
     !---------------------------------------------------------------------------
     ! observation operator (computes H(x)) for specified member
     !---------------------------------------------------------------------------
+    !print *, 'Start Trans_DtoY'
     CALL Trans_DtoY(elem(n),ri,rj,rk,v4d_all,ohx(n))
+    oqc(n)=1
 
+    !---------------------------------------------------------------------------
+    ! Filter out observations that are out of range for the grid
+    !--------------------------------------------------------------------------
+    !print *, "The boundary is lon(1)=",lon(1),"lon(nlon)=",lon(nlon)
+    !print *, "The boundary is lat(1)=",lat(1),"lat(nlat)=",lat(nlat) 
+    if ( rlon(n) < lon(1) .OR. rlon(n) > lon(nlon) ) then
+      oqc(n)=0
+      print *, 'remove number obs n = ', n
+      print *, obid(n),rlon(n),rlat(n)
+    else
+      if ( rlat(n) < lat(1) .OR. rlat(n)>lat(nlat) ) then
+        oqc(n)=0
+        print *, 'remove number obs n = ', n
+        print *, obid(n),rlon(n),rlat(n)
+      end if
+    end if
+
+    !print *, "n=",n,"ohx=",ohx(n)
   enddo ! end do n=1,nobs
 
   !-----------------------------------------------------------------------------
@@ -96,8 +123,10 @@ PROGRAM obsop_drifters
   !-----------------------------------------------------------------------------
   ! Write the observations and their associated innovations to file
   !-----------------------------------------------------------------------------
+  print *, 'Start write_obs2_drifters'
+  !print *, 'otime=', otime
   CALL write_obs2_drifters(obsoutfile,nnobs,elem,rlon,rlat,rlev,obid,oerr,ohx,oqc,otime)
-
+  print *, 'Finish write_obs2_drifters'
  CONTAINS
 !SUBROUTINE process_command_line
 !===============================================================================
